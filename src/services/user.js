749ca0991a877da67a75ce351e40f09d16a8e8eb
Zelesto <theozwane@gmail.com>
2026-06-04 12:17:59 +0200

    Update user API endpoints to remove '/api' prefix 1217

--- src/services/user.js
+++ src/services/user.js
@@ -2,10 +2,10 @@
 import api from "./api"; // your axios instance
 
 const userService = {
-  getAllUsers: () => api.get("/api/users").then(res => res.data),
-  createUser: (user) => api.post("/api/users", user).then(res => res.data),
-  updateUser: (id, user) => api.put(`/api/users/${id}`, user).then(res => res.data),
-  deleteUser: (id) => api.delete(`/api/users/${id}`),
+  getAllUsers: () => api.get("/users").then(res => res.data),
+  createUser: (user) => api.post("/users", user).then(res => res.data),
+  updateUser: (id, user) => api.put(`/users/${id}`, user).then(res => res.data),
+  deleteUser: (id) => api.delete(`/users/${id}`),
 };
 
 export default userService;
