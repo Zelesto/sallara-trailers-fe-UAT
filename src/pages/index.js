26297511d9cf973b3f6ca68629b0fa477d9337fd
Zelesto <theozwane@gmail.com>
2026-06-18 16:17:13 +0200

    Create index.js to export multiple components 1617

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+// src/pages/index.js
+export { default as TripForm } from './TripForm';
+export { default as Dashboard } from './Dashboard';
+export { default as DriverList } from './DriverList';
+export { default as DriverDetails } from './DriverDetails';
+export { default as DriverForm } from './DriverForm';
+export { default as VehicleList } from './VehicleList';
+export { default as VehicleDetails } from './VehicleDetails';
+export { default as VehicleForm } from './VehicleForm';
