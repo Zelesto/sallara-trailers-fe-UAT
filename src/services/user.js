9d5268ec32b75e167edf9d986c6e2e83775733b1
Zelesto <theozwane@gmail.com>
2026-06-29 17:43:36 +0200

    Refactor getAllUsers method in userService 1743
    Refactor getAllUsers to use async/await and add error handling.

--- src/services/user.js
+++ src/services/user.js
@@ -1,11 +1,21 @@
 // src/services/user.js
-import api from "./api"; // your axios instance
+import api from "./api";
 
 const userService = {
-  getAllUsers: () => api.get("/users").then(res => res.data),
+  getAllUsers: async () => {
+    try {
+      const response = await api.get("/users");
+      console.log("User service response:", response);
+      return response.data;
+    } catch (error) {
+      console.error("Error in getAllUsers:", error);
+      throw error;
+    }
+  },
   createUser: (user) => api.post("/users", user).then(res => res.data),
   updateUser: (id, user) => api.put(`/users/${id}`, user).then(res => res.data),
   deleteUser: (id) => api.delete(`/users/${id}`),
+  getUserById: (id) => api.get(`/users/${id}`).then(res => res.data),
 };
 
 export default userService;
