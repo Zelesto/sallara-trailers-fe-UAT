05f9df013d74c95da9a47af8aa6168bb7904d1e4
Zelesto <theozwane@gmail.com>
2026-07-09 10:21:00 +0200

    Update version number from v1.0.2 to v1.0.3 1020

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -152,7 +152,7 @@ const Login = () => {
             fontSize: '0.6rem',
           }}
         >
-          v1.0.2
+          v1.0.3
         </Typography>
 
         <Divider sx={{ width: '100%', mb: 2 }} />
